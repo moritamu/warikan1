d6b5fe6034592c2a760060f855efa7228a328f75
moritamu <moris@r.sannet.ne.jp>
2021-12-08 16:14:15 +0900

    Initial Commit

---
+++ warikan1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warikan1
+//
+//  Created by 森田晋 on 2021/12/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warikan1/warikan1App.swift
@@ -0,0 +1,17 @@
+//
+//  warikan1App.swift
+//  warikan1
+//
+//  Created by 森田晋 on 2021/12/08.
+//
+
+import SwiftUI
+
+@main
+struct warikan1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
