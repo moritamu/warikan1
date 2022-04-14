20263183de1ef466b50e7b3f25124ca0d50df5aa
moritamu <moris@r.sannet.ne.jp>
2022-04-14 17:35:58 +0900

    ok

---
+++ CFDRP/SoundPlayer.swift
@@ -0,0 +1,41 @@
+//
+//  SoundPlayer.swift
+//  MyMusic1
+//
+//  Created by 森田晋 on 2021/11/25.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundPlayer: NSObject {
+    let cymbalData = NSDataAsset(name: "ending")!.data
+    var cymbalPlayer: AVAudioPlayer!
+    
+    let guitarData = NSDataAsset(name: "guitarSound")!.data
+    var guitarPlayer: AVAudioPlayer!
+    
+    func cymbalPlay() {
+//    tryでエラー処理をしろとのエラーが出る
+        do {
+            cymbalPlayer = try AVAudioPlayer(data: cymbalData)
+            
+            cymbalPlayer.play()
+        } catch {
+            print("シンバルでエラー")
+        }
+    }
+
+    func guitarPlay() {
+//    tryでエラー処理をしろとのエラーが出る
+        do {
+            guitarPlayer = try AVAudioPlayer(data: guitarData)
+            
+            guitarPlayer.play()
+        } catch {
+            print("ギターでエラー")
+        }
+    }
+    
+
+}
